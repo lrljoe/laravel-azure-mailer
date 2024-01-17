e66d3feeaf43e8c93abeb8abc15c9d436f8acd29
jliglesias <jliglesias@gmail.com>
2024-01-17 12:12:23 +0100

    update package

--- src/Providers/AzureMailerServiceProvider.php
+++ src/Providers/AzureMailerServiceProvider.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Avantia package.
+ *
+ * (c) Juan Luis Iglesias <jliglesias@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Avantia\Azure\Mailer;
 
 Use AzureMailerTransport;
